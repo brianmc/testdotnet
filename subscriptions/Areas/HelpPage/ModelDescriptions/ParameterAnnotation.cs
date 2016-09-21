6f097276f54764dd9bf4800ed59dfb49b9245db3
bmcmanus <bmcmanus@visa.com>
2016-09-21 14:49:41 -0700

    First Cut

---
+++ subscriptions/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace subscriptions.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ subscriptions/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace subscriptions.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
